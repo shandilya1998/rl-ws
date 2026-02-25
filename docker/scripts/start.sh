2e3338f7b4f0ddbfba2d0ad47918c0bc46f3be02
shandilya1998 <shreyas.shandilya@gmail.com>
2026-02-25 05:33:22 +0000

    RL Training Workflow automation
    Problem
    =======
    1. `.ssh/` directory among unstaged changes, though it should not be
       tracked
    2. Need to automate evaluation and training
    
    Solution
    ========
    1. Added a `.gitignore` with `.ssh/` as the first entry
    2. Added options to start training, evaluation and individual testing.
    
    Note
    ====
    1. Autoformatter corrected indentation in `djinn`

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -2,20 +2,32 @@
 
 name="$2"
 image="$3"
-echo $name
-echo $image
+$platform=$4
+gpus="$5"
+if [[ "$gpus" == "all_gpus" ]]; then
+    gpus="all"
+elif [[ "$gpus" == "no_gpus" ]]; then
+    gpus=0
+else
+    gpus="$4"
+fi
+echo "Container name: $name"
+echo "Image name: $image"
+echo "GPU ID: $gpus"
+echo "Workspace path: $1"
 XAUTH=/tmp/.docker.xauth
-docker run --rm -d -it -v $1:/ws/ \
-	-v ./.ssh:/root/.ssh \
-	-v /dev:/dev \
-	--device-cgroup-rule "c 81:* rmw" \
-	--device-cgroup-rule "c 189:* rmw" \
-	--device /dev/vchiq \
-	-v /run/udev:/run/udev:ro \
-	-e DISPLAY=$DISPLAY \
-	-e XAUTHORITY=$XAUTHORITY \
-	-e QT_GRAPHICSSYSTEM=native \
-	-v $XAUTH:$XAUTH:rw \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	--env="DISPLAY" --net=host \
-	--privileged --name $name $image bash
+docker run -d -it -v $1:/ws/ \
+    -v ./.ssh:/root/.ssh \
+    -v /dev:/dev \
+    --gpus $gpus \
+    --device-cgroup-rule "c 81:* rmw" \
+    --device-cgroup-rule "c 189:* rmw" \
+    --device /dev/vchiq \
+    -v /run/udev:/run/udev:ro \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=$XAUTHORITY \
+    -e QT_GRAPHICSSYSTEM=native \
+    -v $XAUTH:$XAUTH:rw \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --env="DISPLAY" --net=host \
+    --privileged --name $name $image bash
